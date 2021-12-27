826143a62a96462e7658daeff1b190d88878bba9
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-12-27 10:11:29 +0500

    Implemented Admin Login

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const loginRouter = require('./routes/login');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use('/login', loginRouter);
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,47 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const yup = require('yup');
 
 const router = express.Router();
 
-router.post('/student', (req, res) => {
-  res.json({ student: 's1', token: 't1' });
+const schema = yup.object({
+  email: yup.string().required('Email is required').email('Enter a valid email'),
+  password: yup.string().required('Password is required').min(8, 'Password should be at least 8 characters long'),
+});
+
+router.post('/student', async (req, res) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    res.json({ student: 's1', token: 't1' });
+  } catch (e) {
+    res.status(400);
+    res.json({ errors: e.errors });
+  }
 });
 
 router.post('/teacher', (req, res) => {
   res.json({ teacher: 't1', token: 't1' });
 });
 
-router.post('/admin', (req, res) => {
-  res.json({ admin: 'a1', token: 't1' });
+router.post('/admin', async (req, res) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    if (req.body.email !== process.env.ADMIN_EMAIL) {
+      res.status(401);
+      res.json({ errors: ['Admin email is incorrect'] });
+      return;
+    }
+    if (req.body.password !== process.env.ADMIN_PASSWORD) {
+      res.status(401);
+      res.json({ errors: ['Admin password is incorrect'] });
+      return;
+    }
+    const token = jwt.sign({ admin: 'admin' }, process.env.JWT_SECRET);
+    res.json({ admin: 'admin', token });
+  } catch (e) {
+    res.status(400);
+    res.json({ errors: e.errors });
+  }
 });
 
 module.exports = router;
