c76192f575635bee8f325daa57733f3a9b07f167
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2022-01-30 22:49:38 +0500

    Added demo test route

--- routes/others.js
+++ routes/others.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
 const Test = require('../models/test');
+const StatusMessageError = require('../others/StatusMessageError');
 
 const router = express.Router();
 
@@ -13,6 +15,16 @@ router.get('/demo-tests', async (req, res) => {
     res.json({ demoTests });
   } catch (e) {
     res.status(e.status).json({ error: e.message });
+  }
+});
+
+router.get('/demo-tests/:_id', async (req, res) => {
+  try {
+    const result = await Test.findOne({ _id: req.params._id });
+    if (!result) throw new StatusMessageError('Test not found', 404);
+    res.json({ demoTest: result._doc });
+  } catch (e) {
+    res.status(e.status || 500).json({ error: e.message });
   }
 });
 
